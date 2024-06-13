3b92ebddfaad0040b3109e97d683bf762241a253
emir <admin@admin.com>
2024-06-13 15:39:58 +0600

    first commit

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class News(models.Model):
+    title = models.CharField(max_length=228)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    audio = models.FileField(upload_to='media/new_audio')
+
+    def __str__(self):
+        return self.title
